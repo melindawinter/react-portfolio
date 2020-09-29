e18e9203e1e338538b9310921e85996e86924177
Melinda Winter <melindawinter42@gmail.com>
2020-09-29 16:54:09 -0700

    add pages

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-sm mb-5">
+      <h3>Melinda Winter</h3>
+      <ul></ul>
+    </nav>
+  );
+}
+
+export default Header;
