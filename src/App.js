eb8fea0d9a27a674b2d04c793098b26714f7d7fe
Melinda Winter <melindawinter42@gmail.com>
2020-09-30 15:50:55 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import About from "./views/About";
+import Portfolio from "./views/Portfolio";
+import Contact from "./views/Contact";
 
 function App() {
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,16 @@ import "./Header.css";
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-sm mb-5">
-      <h3>Melinda Winter</h3>
-      <ul></ul>
-    </nav>
+    <div>
+      <nav className="navbar navbar-expand-sm mb-5">
+        <h3>Melinda Winter</h3>
+        <ul>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
