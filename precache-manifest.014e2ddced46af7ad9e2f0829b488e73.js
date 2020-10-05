ce960d9eaea369e54725b3a55691fc7f45ecf63c
Melinda Winter <melindawinter42@gmail.com>
2020-10-04 23:25:49 -0700

    Updates

---
+++ precache-manifest.014e2ddced46af7ad9e2f0829b488e73.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d4883e22485b0fe17495f4bc5620ec",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a239f0458b0bcb3e712a",
+    "url": "/react-portfolio/static/css/main.6c283180.chunk.css"
+  },
+  {
+    "revision": "9bef9c36639c4e191fd8",
+    "url": "/react-portfolio/static/js/2.27fb96b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.27fb96b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a239f0458b0bcb3e712a",
+    "url": "/react-portfolio/static/js/main.6682b74a.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "931b0fdd55d651a4ff1dcdd3b0535651",
+    "url": "/react-portfolio/static/media/burger.931b0fdd.png"
+  },
+  {
+    "revision": "9d4a4adc43f95f1b0f92513a3eab815e",
+    "url": "/react-portfolio/static/media/code-quiz.9d4a4adc.png"
+  },
+  {
+    "revision": "3d6d850a6ec2900605cff4f114031cef",
+    "url": "/react-portfolio/static/media/desk.3d6d850a.png"
+  },
+  {
+    "revision": "173ae2af15f476d2165f09766b6afda2",
+    "url": "/react-portfolio/static/media/scheduler.173ae2af.png"
+  },
+  {
+    "revision": "bec2accf47c0bd9560feb4110aab5881",
+    "url": "/react-portfolio/static/media/sw.bec2accf.png"
+  },
+  {
+    "revision": "c8e05b84b4f0182e29ad3510e752eb7b",
+    "url": "/react-portfolio/static/media/teams.c8e05b84.png"
+  },
+  {
+    "revision": "61147cc8302d2df6acc6f381b3b90d8b",
+    "url": "/react-portfolio/static/media/weather.61147cc8.png"
+  }
+]);
